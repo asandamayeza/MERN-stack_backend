128a4986eb7e642cdcc186c8a60828c93ac18e33
asandamayeza <mayezaa32@gmail.com>
2025-01-20 09:19:59 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import movies from './api/movies.route.js';
+
+//initialize express
+const app = express() //create the server
+
+// Attach the cors and express.json middleware that express will use
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api/v1/movies", movies)
+app.use('*', (req,res)=>{
+res.status(404).json({error: "not found"})
+})
+export default app
